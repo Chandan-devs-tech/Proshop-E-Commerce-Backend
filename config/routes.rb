71f78b20782eaa4179a2c511f5082eff94bbd75b
Chandan-devs-tech <chandanguptabsb@gmail.com>
2024-03-25 00:47:59 +0530

    Modify config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resources :users do
+      # resources :users do
         resources :orders, only: [:index, :show, :create, :update, :destroy] do
           resources :payments, only: [:show, :create, :destroy]
           # resources :order_items, only: [:index, :create, :update, :destroy]
@@ -14,7 +14,7 @@ Rails.application.routes.draw do
             resources :products, only: [:index, :show, :create, :update, :destroy]
           end
         end
-      end
+      # end
     end
   end
   devise_for :users, path: 'auth', path_names: {
